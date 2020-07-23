5d76af10afdf47dfae9159d457566aaf69e9cdf0
Won Young <wonyoung989@gmail.com>
2020-07-23 07:02:46 +0200

    Weather Api Interface

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.cityName);
+  const query = req.body.cityName
+  const apiKey = "f95885fc125641d406b13256cb44a43e"
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDiscription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.<h1>");
+      res.write("<h2>The weather is currently " + weatherDiscription + ".<h2>");
+      res.write("<img src=" + imageURL + ">");
+    res.send()
+    })
+
+  })
+
+
+})
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.")
+})
